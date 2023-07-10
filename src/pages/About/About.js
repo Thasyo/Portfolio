936c2f8c200378c43de833b053dc695d0276f2bf
Thasyo Peres <thasyotp2002@gmail.com>
2023-07-10 05:07:23 -0300

    add css from about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,6 +8,9 @@ import js from  "../../assets/js.png"
 import reactJs from  "../../assets/react.png"
 import nodeJs from  "../../assets/node.png"
 import git from  "../../assets/git.png"
+import instagram from  "../../assets/instagram.png"
+import linkedin from  "../../assets/linkedin.png"
+import github from  "../../assets/github.png"
 import { useNavigate } from 'react-router-dom'
 
 const About = () => {
@@ -21,25 +24,35 @@ const About = () => {
     {id: 6, title: "git", image: git},
   ]
 
+  const socialMediasImages = [
+    {id: 1, title: "instagram", image: instagram},
+    {id: 2, title: "linkedin", image: linkedin},
+    {id: 3, title: "github", image: github},
+  ]
+
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className={styles.about}>
 
-      <div className={styles.rightSide}>
-        <img src={img} alt="Foto de Thasyo"  className={styles.img}/>
-        <h2>Thasyo Peres</h2>
-        <p>Desenvolvedor de Software</p>
-      </div>
+      <div className={styles.profileAndHowKnewTheProgrammationWorld}>
 
-      <div>
-
-        <div>
-          <h1 className={styles.title}>Como conheci o mundo da programação?</h1>
+        <div className={styles.rightSide}>
+          <img src={img} alt="Foto de Thasyo"  className={styles.img}/>
+          <h2>Thasyo Peres</h2>
+          <p>Desenvolvedor de Software</p>
         </div>
 
-        <div className={styles.description_title}>
-          <p><Icon icon="el:star" className={styles.star}/> Olá! Meu nome é Thasyo, sou desenvolvedor de software. Adentrei nesse mundo de tecnologia em meados de 2020, durante a pandemia, no qual tive a oportunidade de conhecer melhor a internet e suas funcionalidades. A partir disso, conheci o universo do desenvolvimento de softwares, e fiquei apaixonado em como as coisas se estruturavam e se encaixavam. Então, por meio disso, iniciei meus estudos e me profissionalizei na área.</p>  
+        <div className={styles.howKnewTheProgrammationWorld}>
+
+          <div>
+            <h1 className={styles.title}>Como conheci o mundo da programação?</h1>
+          </div>
+
+          <div className={styles.description_title}>
+            <p><Icon icon="el:star" className={styles.star}/> Olá! Meu nome é Thasyo, sou desenvolvedor de software. Adentrei nesse mundo de tecnologia em meados de 2020, durante a pandemia, no qual tive a oportunidade de conhecer melhor a internet e suas funcionalidades. A partir disso, conheci o universo do desenvolvimento de software, e fiquei apaixonado em como as coisas se estruturavam e se encaixavam. Então, por meio disso, iniciei meus estudos e me profissionalizei na área.</p>
+          </div>
+
         </div>
 
       </div>
@@ -52,10 +65,10 @@ const About = () => {
 
         <div className={styles.description_title}>
           <ul>
-            <li><Icon icon="el:star" className={styles.star}/><span>Comunicação: Tenho ótima capacidade de me comunicar e saber me expressar com facilidade, além de saber ouvir e entender.</span></li>
-            <li><Icon icon="el:star" className={styles.star}/><span>Pensamento crítico: Tenho habilidade de analisar e avaliar situações, e a partir disso, conseguir absorver as melhoras soluções.</span></li>
-            <li><Icon icon="el:star" className={styles.star}/><span>Adaptabilidade: Tenho flexibilidade para me ajustar a novas situações e lidar com mudanças.</span></li>
-            <li><Icon icon="el:star" className={styles.star}/><span>Empatia: Sempre tento me colocar no lugar do próximo, tentando sempre tomar as melhores decisões, com cautela, para ambas as partes.</span></li>
+            <li><Icon icon="el:star" className={styles.star}/><span><strong>Comunicação:</strong> Tenho ótima capacidade de me comunicar e saber me expressar com facilidade, além de saber ouvir e entender.</span></li>
+            <li><Icon icon="el:star" className={styles.star}/><span><strong>Pensamento crítico:</strong> Tenho habilidade de analisar e avaliar situações, e a partir disso, conseguir absorver as melhoras soluções.</span></li>
+            <li><Icon icon="el:star" className={styles.star}/><span><strong>Adaptabilidade:</strong> Tenho flexibilidade para me ajustar a novas situações e lidar com mudanças.</span></li>
+            <li><Icon icon="el:star" className={styles.star}/><span><strong>Empatia:</strong> Sempre tento me colocar no lugar do próximo, tentando sempre tomar as melhores decisões, com cautela, para ambas as partes.</span></li>
           </ul>
         </div>
 
@@ -69,6 +82,20 @@ const About = () => {
 
         <div>
           {hardSkillsImages.map((techImage) => (
+            <img key={techImage.id} src={techImage.image} alt={techImage.title}/>
+          ))}
+        </div>
+
+      </div>
+
+      <div>
+
+        <div>
+          <h1 className={styles.title}>Redes Sociais</h1>
+        </div>
+
+        <div>
+          {socialMediasImages.map((techImage) => (
             <img key={techImage.id} src={techImage.image} alt={techImage.title}/>
           ))}
         </div>
